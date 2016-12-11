0a86449aad30d61075226717921de56f33d47457
kancherla.surendra@gmail.com <kancherla.surendra@gmail.com>
2016-12-11 21:31:32 +0530

    Initial commit

--- app.js
+++ app.js
@@ -15,11 +15,13 @@ app.post("/login", function(req, res) {
 	res.end("Login successful.");
 });
 
-var port = process.env.port || 8080;
+var server_port = process.env.YOUR_PORT || process.env.PORT || 8080;
+var server_host = process.env.YOUR_HOST || '0.0.0.0';
 
-app.listen(port, function() {
-	console.log("Server listening on port 8080");
+app.listen(server_port, server_host, function() {
+	console.log("Server started on port 8080");
 });
+
 
 
 
